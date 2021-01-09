737ab9f85b0f27e518d7d26f1f1f7d2fec3a72b5
Serge Merzliakov <smerzlia@gmail.com>
2021-01-09 13:41:17 +1100

    Log JVM arguments. Simplified demo code.

--- src/main/java/org/tinydemo/Application.java
+++ src/main/java/org/tinydemo/Application.java
@@ -3,9 +3,8 @@ package org.tinydemo;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.io.File;
-import java.util.LinkedList;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 import java.util.List;
 
 public class Application {
@@ -14,18 +13,19 @@ public class Application {
     
     public static void main(String[] args) {
         System.out.println("\n****** Start Tiny Application ******");
-        File f = new File("/usr/home/blah");
-        System.out.println(f.getPath());
-        
-        List<String> items = new LinkedList<>();
-        items.add("Hello");
-        items.add("World");
 
-        System.out.println(logger.getName());
-        System.out.print(items.get(0));
-        System.out.print(" ");
-        System.out.println(items.get(1));
+        RuntimeMXBean runtimeMxBean = ManagementFactory.getRuntimeMXBean();
+        List<String> arguments = runtimeMxBean.getInputArguments();
 
+        System.out.println("\n-----------------------");
+        System.out.println("\nJVM Arguments:\n");
+        for(String s : arguments)
+            System.out.println(s);
+        System.out.println("\n-----------------------\n");
+
+        System.out.println();
+        System.out.println("This is a tiny application which does nothing");
+        System.out.println();
         System.out.println("****** Tiny Application Stopped ******");
     }
 }
